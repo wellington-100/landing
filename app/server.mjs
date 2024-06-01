52432ff02afdc504a3190e2751b16897ce61fed3
Wellington-100 <vasile.andruscenco@gmail.com>
2024-06-01 20:35:37 +0300

    HW2: add and process new jasonData(tags)

--- app/server.mjs
+++ app/server.mjs
@@ -1,30 +1,57 @@
 import http from 'node:http'
 import fs from 'node:fs'
-// 2. create the server object
 
+// create the server object
 
-// HW: modify the HTML file and server logic 
-//     so you load a favicon / logo into the page
+const routes = {
+    '/': 'index.html',
+    '/style.css': 'css/style.css',
+    '/fav.png': 'images/fav.png',
+    '/jsbook.png': 'images/jsbook.png',
+}
 
+// HW1*: logic with XML storage data
+// HW2: adding a new jsonData: ["JS", "learning", "education", "2022", "programming", "IT"] -> refactor the code
 const server = http.createServer((req, res) => {
    
-    if (req.url == '/') {
-        // static file serving
-        fs.readFile('index.html', (err, data) =>{
-            res.write(data)
-            res.end()
-        })
-    } else if (req.url == '/style.css') {
-        // static file serving
-        fs.readFile('style.css', (err, data) => {
-            res.write(data)
-            res.end()
-        })
-    } else if (req.url == '/fav.png') {
-        // static file serving
-        fs.readFile('fav.png', (err, data) => {
-            res.write(data)
-            res.end()
+    if (routes[req.url]) {
+        fs.readFile(routes[req.url], (err, data) =>{
+            
+            // react only for the main page
+            if (routes[req.url] == 'index.html'){
+
+                //load the product from the .json
+                fs.readFile('data/product.json', (err, jsonData) => {
+                    let productData = JSON.parse(jsonData)
+                    //replace the date in the html template
+                    data = data.toString()
+                    
+                    // prepare the login tags rendering
+                    let tagsArray = productData['tags']
+                    for (let i = tagsArray.length - 1; i > 0; i--){
+                        let x = Math.floor(Math.random() * (i + 1));
+                        let tempTag = tagsArray[i]
+                        tagsArray[i] = tagsArray[x]
+                        tagsArray[x] = tempTag
+                    }
+                    let stringTags = tagsArray.map(tag => `<a href="/">#${tag}</a>`).join(' ')
+
+                    // Server side rendering(SSR)
+                    data = data.replace('{title}', productData['title'])
+                    data = data.replace('{image}', productData['image'])
+                    data = data.replace('{subtitle}', productData['subtitle'])
+                    data = data.replace('{description}', productData['description'])
+                    data = data.replace('{priceAmount}', productData['price']['amount'])
+                    data = data.replace('{priceCurrency}', productData['price']['currency'])
+                    data = data.replace('{tags}', stringTags)
+                    
+                    res.write(data)
+                    res.end()
+                })
+            } else {
+                res.write(data)
+                res.end()
+            }
         })
     } else {
         res.write('not found')
