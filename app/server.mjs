6e5e5f4cd27c96d74885299861b70b57c6bc1cc8
Wellington-100 <vasile.andruscenco@gmail.com>
2024-06-03 02:50:03 +0300

    jsonData => DOM

--- app/server.mjs
+++ app/server.mjs
@@ -1,8 +1,6 @@
 import http from 'node:http'
 import fs from 'node:fs'
 
-
-
 const routes = {
     '/': 'index.html',
     '/style.css': 'css/style.css',
@@ -14,42 +12,13 @@ const server = http.createServer((req, res) => {
    
     if (routes[req.url]) {
         fs.readFile(routes[req.url], (err, data) =>{
-            
-            // react only for the main page
-            if (routes[req.url] == 'index.html'){
-
-                //load the product from the .json
-                fs.readFile('data/product.json', (err, jsonData) => {
-                    let productData = JSON.parse(jsonData)
-                    //replace the date in the html template
-                    data = data.toString()
-                    
-                    // prepare the logic tags rendering
-                    let tagsArray = productData['tags']
-                    for (let i = tagsArray.length - 1; i > 0; i--){
-                        let x = Math.floor(Math.random() * (i + 1));
-                        let tempTag = tagsArray[i]
-                        tagsArray[i] = tagsArray[x]
-                        tagsArray[x] = tempTag
-                    }
-                    let stringTags = tagsArray.map(tag => `<a href="/">#${tag}</a>`).join(' ')
-
-                    // Server side rendering(SSR)
-                    data = data.replace('{title}', productData['title'])
-                    data = data.replace('{image}', productData['image'])
-                    data = data.replace('{subtitle}', productData['subtitle'])
-                    data = data.replace('{description}', productData['description'])
-                    data = data.replace('{priceAmount}', productData['price']['amount'])
-                    data = data.replace('{priceCurrency}', productData['price']['currency'])
-                    data = data.replace('{tags}', stringTags)
-                    
-                    res.write(data)
-                    res.end()
-                })
-            } else {
-                res.write(data)
-                res.end()
-            }
+            res.write(data)
+            res.end()
+        })
+    } else if (req.url == '/api/product'){
+        fs.readFile('data/product.json', (err, jsonData) => {
+            res.write(jsonData)
+            res.end()
         })
     } else {
         res.write('not found')
