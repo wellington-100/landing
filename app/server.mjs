3f5d8d8a32667aa352146d2d9f0afb82ea3b5e7c
Wellington-100 <vasile.andruscenco@gmail.com>
2024-06-02 23:46:33 +0300

    alternative xml storage

--- app/server.mjs
+++ app/server.mjs
@@ -31,7 +31,7 @@ const server = http.createServer((req, res) => {
                         // prepare the logic tags mixing
                         let tagsArray = productData.tags[0].tag
                         for (let i = tagsArray.length - 1; i > 0; i--){
-                            let x = Math.floor(Math.random() * (i + 1));
+                            let x = Math.floor(Math.random() * (i + 1))
                             let tempTag = tagsArray[i]
                             tagsArray[i] = tagsArray[x]
                             tagsArray[x] = tempTag
@@ -39,13 +39,13 @@ const server = http.createServer((req, res) => {
                         let stringTags = tagsArray.map(tag => `<a href="/">#${tag}</a>`).join(' ')
     
                         // Server side rendering(SSR)
-                        data = data.replace('{title}', productData.title[0]);
-                        data = data.replace('{image}', productData.image[0]);
-                        data = data.replace('{subtitle}', productData.subtitle[0]);
-                        data = data.replace('{description}', productData.description[0]);
-                        data = data.replace('{priceAmount}', productData.price[0].amount[0]);
-                        data = data.replace('{priceCurrency}', productData.price[0].currency[0]);
-                        data = data.replace('{tags}', stringTags);
+                        data = data.replace('{title}', productData.title[0])
+                        data = data.replace('{image}', productData.image[0])
+                        data = data.replace('{subtitle}', productData.subtitle[0])
+                        data = data.replace('{description}', productData.description[0])
+                        data = data.replace('{priceAmount}', productData.price[0].amount[0])
+                        data = data.replace('{priceCurrency}', productData.price[0].currency[0])
+                        data = data.replace('{tags}', stringTags)
 
                         
                         res.write(data)
